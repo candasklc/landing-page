3fb716c7e14c4e066e37eaa8230511d1b34a11ab
TAAKICA1 <candas.kilic@swisscom.com>
2021-10-26 03:29:27 +0200

    First steps of product landing page is created.

---
+++ index.js
@@ -0,0 +1,16 @@
+function cardCreator(){
+    for (let i = 0; i < 5; i++) {
+        const parentElement = document.getElementById('card-container');
+        const cardElement = document.createElement('div');
+        cardElement.className = 'card';
+        parentElement.appendChild(cardElement);   
+    }
+}
+
+
+
+
+
+
+// Call functions below, in order.
+cardCreator();
